0d9a8584145bae4f3bdb3e2a296a5ee8c9da6fe3
jason.gong <gongjun116403737@163.com>
2019-05-19 14:52:51 +0800

    fix ball bounce bug

--- demo_01_bouncy_ball/bouncy_ball.py
+++ demo_01_bouncy_ball/bouncy_ball.py
@@ -3,7 +3,8 @@ from pygame.locals import *
 pygame.init()
 
 size = width, height = 500, 500
-move = [0, 0]
+
+distance = [0,0]
 speed = [10,0.5]
 gravity = [0,0.5]
 black = 0, 0, 0
@@ -11,7 +12,13 @@ screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("football.png")
 ballrect = ball.get_rect()
-pressed =0;
+pressed=0
+
+BOUNCE=2
+ROLL=1
+STOP=0
+state = BOUNCE
+
 
 while 1:
     for event in pygame.event.get():
@@ -27,21 +34,41 @@ while 1:
     mouse = pygame.mouse.get_rel()
     if pressed:
         speed=[10,0.5]
+        state = BOUNCE
+        distance =[0,0]
         ballrect = ballrect.move(mouse)
     else:
-        speed[0] += gravity[0]
-        speed[1] += gravity[1]
+        if state == BOUNCE:
+            if speed[1] < 5 and speed[1] >-5 and ballrect.bottom > height - 5:
+                state=ROLL
+                distance[1]=0
+        if state == ROLL:
+            if speed[0] < 0.5 and speed[0] > -0.5:
+                state=STOP
+
+
+        if state == BOUNCE:
+            speed[0] += gravity[0]
+            speed[1] += gravity[1]
             
-        if speed[1] < 1 and ballrect.bottom > (height-5):
+            distance[0] = speed[0]
+            distance[1] = speed[1]
+
+            if ballrect.left < 0 or ballrect.right > width:
+                speed[0] = -speed[0]*0.9
+                distance[0] = -distance[0]
+            if ballrect.top < 0 or ballrect.bottom > height:
+                speed[1] = -speed[1]*0.8
+                distance[1] = -distance[1]
+            ballrect = ballrect.move(distance)
+    
+        if state == ROLL:
             speed[0] = speed[0]*0.95
-        
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]*0.9
-            print("rect",ballrect,move,speed)
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]*0.9
-            print("rect",ballrect,move,speed)
+            distance[0] = speed[0]
+            if ballrect.left < 0 or ballrect.right > width:
+                speed[0] = -speed[0]
+                distance[0] = -distance[0]
+            ballrect = ballrect.move(distance)
 
     screen.fill(black)
     ## copy ball image to display area
